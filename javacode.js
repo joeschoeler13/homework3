79865cfb738a55964708458cf1a6afdaa7cc1837
Neuer Nutzer <neuernutzer@Marcuss-MacBook-Pro.local>
2017-09-26 21:48:27 +0200

    The function inserts a string with my global variable at the beginning of the list, which is good. The problem is, the function does that always, when the button is clicked, so that in the end the first li-element of the ul will just be an endless string with n-times the sub-srting 'Your list is N item/s long'. I have to find something to always delete the first element of the list, when the function is started.

--- javacode.js
+++ javacode.js
@@ -7,8 +7,9 @@ function AddAnimal() {
 
   document.getElementById("list1").insertAdjacentHTML('beforeend', item);
   
-  count = count + 1
-  alert(count)
+  count = count + 1;
+  var textcount = "Your list is " + count + " item/s long.";
+  document.getElementById("list1").insertAdjacentHTML('afterbegin', textcount);
   
   document.getElementById('input1').value = "";
 }
