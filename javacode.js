c6c541ed00f482f9de46441a9c510819301e49e3
Neuer Nutzer <neuernutzer@Marcuss-MacBook-Pro.local>
2017-09-26 20:58:01 +0200

    After our class with them, Andy posted some code on slack #general , which could add an item to the list. I took this code and incorporated it in my list. The method works. When I now run the program it adds an item to the list. The next and last step will be to insert a count of the length of the list and to update it.

--- javacode.js
+++ javacode.js
@@ -1,6 +1,9 @@
 function AddAnimal() {
-
-var item = document.getElementById('input1').value
-alert(item)
-document.getElementById('input1').value = ""
+    
+      var animal = document.getElementById('input1').value;
+      var item = "<li>" + animal + "</li>";
+    
+      document.getElementById("list1").insertAdjacentHTML('beforeend', item);
+    
+      document.getElementById('input1').value = "";
 }
