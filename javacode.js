8dbc248a3c1271c27ac1492ed3a9dc342bd3f4a0
Neuer Nutzer <neuernutzer@Marcuss-MacBook-Pro.local>
2017-09-27 00:42:24 +0200

    JSFIDDLE-CODE-ONLY! I finally made it! The number gets now shown right above the list. It is updated every time a new animal is added and all that without showing all the substrings. I found this 'span' solution for my variable here: https://stackoverflow.com/questions/40858456/how-to-display-a-javascript-var-in-html-body . Last step is now to let Github host this webpage.

--- javacode.js
+++ javacode.js
@@ -6,10 +6,8 @@ function AddAnimal() {
   var item = "<li>" + animal + "</li>";
 
   document.getElementById("list1").insertAdjacentHTML('beforeend', item);
+  document.getElementById('input1').value = "";
 
   count = count + 1;
-  var textcount = "UPDATE! Number of animals in your list: " + count + " ";
-  document.getElementById("list1").insertAdjacentHTML('afterbegin', textcount);
-
-  document.getElementById('input1').value = "";
+  document.getElementById('span1').innerHTML = count
 }
