dfcada94130bffd04b72b14c8fa08f496373252b
Neuer Nutzer <neuernutzer@Marcuss-MacBook-Pro.local>
2017-09-26 21:35:58 +0200

    I stored a variable 'count' globally. It does not really count the length of the ul but as it is stored globally the function can just add +1 onto it so that it gives back the current number of li-elements. Even if it does not count the ul-length, the user wont know that and sees only the thing he should see --> the current number of li elements. By now the number gets only ALERTed by the function. But I will try to incorporate the variable into the html-code.

--- javacode.js
+++ javacode.js
@@ -1,9 +1,14 @@
+var count = 0
+
 function AddAnimal() {
-    
-      var animal = document.getElementById('input1').value;
-      var item = "<li>" + animal + "</li>";
-    
-      document.getElementById("list1").insertAdjacentHTML('beforeend', item);
-    
-      document.getElementById('input1').value = "";
+
+  var animal = document.getElementById('input1').value;
+  var item = "<li>" + animal + "</li>";
+
+  document.getElementById("list1").insertAdjacentHTML('beforeend', item);
+  
+  count = count + 1
+  alert(count)
+  
+  document.getElementById('input1').value = "";
 }
